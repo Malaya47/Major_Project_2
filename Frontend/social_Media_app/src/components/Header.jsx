1b205337a29bcdb5ef8b58b69571273effab38d1
Malaya Tiwari <sidzilegend@gmail.com>
2024-11-18 19:01:12 +0530

    fixed header

--- Frontend/social_Media_app/src/components/Header.jsx
+++ Frontend/social_Media_app/src/components/Header.jsx
@@ -26,38 +26,20 @@ const Header = () => {
             className="collapse navbar-collapse justify-content-between"
             id="navbarNav"
           >
-            <div className="d-flex justify-content-center w-100">
-              <div
-                className="d-flex"
-                style={{ maxWidth: "600px", width: "100%" }}
-              >
-                <input
-                  // onChange={}
-                  className="form-control"
-                  type="search"
-                  placeholder="Search"
-                  aria-label="Search"
-                />
-              </div>
-            </div>
             <ul className="navbar-nav ms-auto d-flex align-items-center">
               <li className="nav-item">
                 <NavLink
-                  to={"/"}
+                  to={"/login"}
                   className="btn btn-secondary"
                   aria-current="page"
                 >
                   Login
                 </NavLink>
               </li>
+
               <li className="nav-item ms-3">
-                <NavLink className="nav-link text-light" to="/">
-                  Wishlist
-                </NavLink>
-              </li>
-              <li className="nav-item ms-3">
-                <NavLink className="nav-link text-light" to="/">
-                  Avatar
+                <NavLink className="nav-link text-light" to="/mainProfile">
+                  Profile
                 </NavLink>
               </li>
             </ul>
