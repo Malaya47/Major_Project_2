690d36f917340888d0b165bb62ad3ffdcea536ff
Malaya Tiwari <sidzilegend@gmail.com>
2024-11-18 19:06:11 +0530

    completed right view

--- Frontend/social_Media_app/src/components/RightView.jsx
+++ Frontend/social_Media_app/src/components/RightView.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import SuggestionListCard from "./SuggestionListCard";
+import ProfilePage from "../pages/ProfilePage";
 
 const RightView = () => {
+  const [searchName, setSearchName] = useState("");
+
+  const searchHandler = (e) => {
+    setSearchName(e.target.value);
+  };
+
   return (
     <>
       <section
-        className="d-none d-lg-block border border-dark bg-body-tertiary p-3 position-fixed"
+        className="d-none d-lg-block border border-dark bg-body-tertiary p-3 position-fixed right-view"
         style={{
           width: "275px",
           height: "calc(100vh - 60px)", // Adjusts height to accommodate fixed position
@@ -16,25 +24,19 @@ const RightView = () => {
           <button className="btn btn-dark mb-2 px-4">Trending</button>
           <button className="btn btn-dark mb-2 px-4">Latest</button>
         </div>
-
-        <p className="fw-bold">Suggestions for you</p>
-        <div className="d-flex align-items-center p-2 border rounded bg-light mb-2">
-          <div className="me-3">
-            <img
-              style={{ width: "50px" }}
-              className="img-fluid"
-              src="https://media.istockphoto.com/id/1337144146/vector/default-avatar-profile-icon-vector.jpg?s=1024x1024&w=is&k=20&c=6XEZlH2FjqdpXUqjUK4y0LlWF6yViZVWn9HZJ-IR8gU="
-              alt="avatar"
-            />
-          </div>
-          <div className="flex-grow-1">
-            <div className="fw-bold">John Doe</div>
-            <div className="text-muted">@JohnDoe</div>
-          </div>
-          <div>
-            <button className="btn btn-dark btn-sm">Follow</button>
-          </div>
+        <div className="d-flex">
+          <input
+            onChange={searchHandler}
+            className="form-control mb-3 mt-3"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+            value={searchName}
+          />
         </div>
+        <p className="fw-bold">Suggestions for you</p>
+
+        <SuggestionListCard searchName={searchName} />
       </section>
     </>
   );
