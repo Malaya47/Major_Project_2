3037ef07bc129f6748cb181b03b854df351b0bb6
Malaya Tiwari <sidzilegend@gmail.com>
2024-11-18 18:59:50 +0530

    made route protection

--- Frontend/social_Media_app/src/main.jsx
+++ Frontend/social_Media_app/src/main.jsx
@@ -8,12 +8,48 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import store from "./app/store.js";
 import { Provider } from "react-redux";
-
+import ProfilePage from "./pages/ProfilePage.jsx";
+import ExplorePage from "./pages/ExplorePage.jsx";
+import BookmarkPage from "./pages/BookmarkPage.jsx";
+import MainProfilePage from "./pages/MainProfilePage.jsx";
+import LogInForm from "./pages/LogInForm.jsx";
+import RegistrationForm from "./pages/RegistrationForm.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <ProtectedRoute />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "/profile/:name",
+        element: <ProfilePage />,
+      },
+      {
+        path: "/explore",
+        element: <ExplorePage />,
+      },
+      {
+        path: "/bookmark",
+        element: <BookmarkPage />,
+      },
+      {
+        path: "/mainProfile",
+        element: <MainProfilePage />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <LogInForm />,
+  },
+  {
+    path: "/register",
+    element: <RegistrationForm />,
   },
 ]);
 
