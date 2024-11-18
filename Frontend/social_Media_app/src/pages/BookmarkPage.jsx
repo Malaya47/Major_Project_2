ffdfdc33058a0cc1e1b341b44627433ca1edeaf4
Malaya Tiwari <sidzilegend@gmail.com>
2024-11-18 19:06:50 +0530

    made bookmark

---
+++ Frontend/social_Media_app/src/pages/BookmarkPage.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Header from "../components/Header";
+import LeftView from "../components/LeftView";
+import RightView from "../components/RightView";
+import { useGetBookmarkedPostsQuery } from "../features/apiSlice";
+import PostCard from "../components/PostCard";
+
+const BookmarkPage = () => {
+  const { data, refetch } = useGetBookmarkedPostsQuery();
+
+
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-3">
+            <LeftView />
+          </div>
+          <div className="col-md-6" style={{ marginTop: "100px" }}>
+            <div className="">
+              {data?.bookmarks.map((post) => (
+                <div key={post._id}>
+                  <PostCard
+                    data={{
+                      user: {
+                        name: post.userId?.name,
+                        userName: post.userId?.userName,
+                        profileImage: post.userId?.profileImage,
+                        posts: [post], // Wrap the single post in an array to match structure
+                      },
+                    }}
+                    refetch={refetch}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="col-md-3">
+            <RightView />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BookmarkPage;
