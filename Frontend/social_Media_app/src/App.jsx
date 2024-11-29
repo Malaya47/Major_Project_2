0dbef88c6220b8454b4c779cc82b8d14e734a820
Malaya Tiwari <sidzilegend@gmail.com>
2024-11-30 00:55:36 +0530

    update: fixed spacing and packages

--- Frontend/social_Media_app/src/App.jsx
+++ Frontend/social_Media_app/src/App.jsx
@@ -4,22 +4,18 @@ import View from "./components/View";
 import Footer from "./components/Footer";
 
 function App() {
-
-  
-
   return (
     <>
-    <div style={{ backgroundColor: "#16181c", minHeight: "calc(100vh - 60px)"}} className=" text-light">
-      <Header />
-      <View />
-    </div>
-    <Footer />
+      <div
+        style={{ backgroundColor: "#16181c", minHeight: "calc(100vh - 60px)" }}
+        className=" text-light"
+      >
+        <Header />
+        <View />
+      </div>
+      <Footer />
     </>
   );
 }
-
-
-
-
 
 export default App;
