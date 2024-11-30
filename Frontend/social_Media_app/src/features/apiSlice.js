cd59dfc5fbdcb959b80369e0c2277e4219973eef
Malaya Tiwari <sidzilegend@gmail.com>
2024-11-30 19:09:18 +0530

    fix: small change

--- Frontend/social_Media_app/src/features/apiSlice.js
+++ Frontend/social_Media_app/src/features/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   reducerPath: "apiSlice",
-  baseQuery: fetchBaseQuery({ baseUrl: `${import.meta.env.VITE_API_URL}` }),
+  baseQuery: fetchBaseQuery({ baseUrl: `${import.meta.env.VITE_API_URL}/` }),
   endpoints: (builder) => ({
     getProfileUser: builder.query({
       query: (id) => `profileUser/${id}`,
